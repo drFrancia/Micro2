a83a312bdfd4300771881e18da4943e33fe7c309
Sebastian <fsinsfran@gmail.com>
2024-09-16 10:51:17 -0400

    cambios varios en el codigo + solucion de errores

--- services/auth-service/src/app.py
+++ services/auth-service/src/app.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
-from src.auth_routes.auth_routes import auth_blueprint
-from config.config import Config
+from .auth_routes.auth_routes import auth_blueprint
+from .config.config import Config
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app. config.from_object(Config)
+app.config.from_object(Config)
 
 jwt = JWTManager(app)
 db = SQLAlchemy(app)
 
 app.register_blueprint(auth_blueprint, url_prefix = '/auth')
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0' port=5001) #falta el metodo run kkkkk
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
--- services/auth-service/src/config/config.py
+++ services/auth-service/src/config/config.py
@@ -5,5 +5,5 @@ db = SQLAlchemy()
 
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret!'
-    AQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL_AUTH') or f'postgresql://sebastian:7501095@127.0.0.1:5432/{'Databases'}/products_db' ## Preguntar si se puede haccer '/databases/product_db'
+    AQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL_AUTH') or f'postgresql://sebastian:7501095@127.0.0.1:5432/Databases' ## Preguntar si se puede haccer '/databases/product_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
